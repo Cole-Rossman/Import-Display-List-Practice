04d863f2fa597b23aa325f2cdd680d201f2c12a8
Cole Rossman <colerossman99@gmail.com>
2022-02-01 10:10:24 -0800

    images added to assets and starwars.js object created

---
+++ starwars.js
@@ -0,0 +1,26 @@
+export const starWarsCharacters = [{
+    name: 'Darth-Vader',
+    affiliation: 'sith',
+    dangerLevel: 10,
+    image: './assets/darth-vader.png',
+
+},
+{
+    name: 'Yoda',
+    affiliation: 'jedi',
+    dangerLevel: 10,
+    image: './assets/yoda.png', 
+},
+{
+    name: 'Boba-Fett',
+    affiliation: 'bounty hunter',
+    dangerLevel: 8,
+    image: './assets/boba-fett.png', 
+},
+{
+    name: 'Leia',
+    affiliation: 'rebel alliance',
+    dangerLevel: 7,
+    image: './assets/leia.png', 
+},
+];

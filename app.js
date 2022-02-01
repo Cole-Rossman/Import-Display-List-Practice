241c566a09a279868fd322bc4e6155035264fd3a
Cole Rossman <colerossman99@gmail.com>
2022-02-01 12:49:24 -0800

    in app.js array and render function imported, container elements grabbed, new functions created in app.js

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 // import functions and grab DOM elements
+import { renderCharacter, renderCar } from './utils.js';
+import { characters } from './starwars.js';
+import { cars } from './cars.js';
 
+const divInfo = document.getElementById('character-info');
+const ulInfo = document.getElementById('cars-list');
+
+
+function starWarsFunction() {
+    for (const character of characters) {
+        const div = renderCharacter(character);
+        divInfo.append(div);
+    }
+}
+
+function carFunction() {
+    for (const car of cars) {
+        const ul = renderCar(car);
+        ulInfo.append(ul);
+    }
+}
+
+carFunction();
+starWarsFunction();
 // let state
 
 // set event listeners 

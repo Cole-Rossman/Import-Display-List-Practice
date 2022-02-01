45eda524377c44e978157d5409b89c2dffe48590
Cole Rossman <colerossman99@gmail.com>
2022-02-01 15:10:19 -0800

    trees.js added with data

---
+++ trees.js
@@ -0,0 +1,18 @@
+export const trees = [
+    {
+        name: 'Douglas fir',
+        states: ['Oregon', 'Washington', 'Idaho', 'Utah', 'Nevada'],
+        lifespan: '500-1000',
+    },
+    {
+        name: 'Palm tree',
+        states: ['California', 'Hawaii', 'Arizona', 'Georgia', 'Florida'],
+        lifespan: '70-100',
+    },
+    {
+        name: 'Red maple',
+        states: ['Maine', 'Minnesota', 'Michigan', 'North Carolina', 'Wisconsin'],
+        lifespan: '80-100',
+    },
+
+];

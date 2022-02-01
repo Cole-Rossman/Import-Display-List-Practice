1b9b4b684eb0499c7e942dc7e453f5c3f4fb76bf
Cole Rossman <colerossman99@gmail.com>
2022-02-01 14:09:04 -0800

    sodas.js data added

---
+++ sodas.js
@@ -0,0 +1,36 @@
+export const sodas = [
+    {
+        name: 'Pepsi',
+        nutrition: {
+            calories: 150,
+            servingSize: '355mL',
+            sugars: '41g',
+        },
+    },
+    {
+        name: 'Coke',
+        nutrition: {
+            calories: 140,
+            servingSize: '355mL',
+            sugars: '39g', 
+        },
+    },
+    {
+        name: 'Sprite',
+        nutrition: {
+            calories: 192,
+            servingSize: '355mL',
+            sugars: '44g',
+        },
+
+    },
+    {
+        name: 'Fanta',
+        nutrition: {
+            calories: 160,
+            servingSize: '355mL',
+            sugars: '42g',
+        },
+    },
+
+];
